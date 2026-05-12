315533ad73c6a56e583b0a7db726fa4cf5c388d6
Shashuvo <shahariat2810@gmail.com>
2026-05-12 16:51:36 +0600

    builded a server with nodejs

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import { IncomingMessage, Server, createServer } from "http";
+
+
+const server: Server = createServer((req: IncomingMessage, res) => {
+    console.log(req);
+});
+
+server.listen(5000, () => {
+    console.log("Server is running on port 5000.");
+});
