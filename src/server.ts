03f12c86c321fa723d3b5cd9de296c100fc09c6b
Shashuvo <shahariat2810@gmail.com>
2026-05-12 17:14:31 +0600

    Route handled with HTTP requests and responses

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,22 @@
-import { IncomingMessage, Server, createServer } from "http";
+import { IncomingMessage, Server, ServerResponse, createServer } from "http";
 
 
-const server: Server = createServer((req: IncomingMessage, res) => {
-    console.log(req);
+const server: Server = createServer((req: IncomingMessage, res: ServerResponse) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === "/" && method === "GET") {
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({ message: "This is Root Route" }));
+    }
+    else if (url?.startsWith("/products")) {
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({ message: "This is Products Route" }));
+    }
+    else {
+        res.writeHead(404, { "content-type": "application/json" });
+        res.end(JSON.stringify({ message: "Route not found" }));
+    }
 });
 
 server.listen(5000, () => {
