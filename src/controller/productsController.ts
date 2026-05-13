0640cef0db4f003ed39d047832534f01680ea40c
Shashuvo <shahariat2810@gmail.com>
2026-05-13 23:34:33 +0600

    builded GET method operations

--- src/controller/productsController.ts
+++ src/controller/productsController.ts
@@ -1,15 +1,32 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../services/productsServices";
+import type { Products } from "../types/productsType";
 
 export const productsController = async (req: IncomingMessage, res: ServerResponse) => {
     const url = req.url;
     const method = req.method;
+
+    const urlParts = url?.split("/");
+    const id = urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
+
+
+    // GET all products
     if (url === "/products" && method === "GET") {
         const products = readProduct();
         res.writeHead(200, { "content-type": "application/json" });
         res.end(JSON.stringify({
             message: "This is Products Route",
             data: products
+        }));
+    }
+    // GET single product
+    else if(method==="GET" && id !== null){
+        const products = readProduct();
+        const product = products.find((p : Products)=>p.id === id);
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({
+            message: "This is Product Route",
+            data: product
         }));
     }
 }
---
+++ src/types/productsType.ts
@@ -0,0 +1,8 @@
+export interface Products {
+    id: number;
+    name: string;
+    category: string;
+    price: number;
+    stock: number;
+    brand: string;
+}
